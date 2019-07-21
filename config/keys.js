f7a95e1813d0a9056a92a6eed562150f79a62b9c
Praveen <praveen_kumaresan@outlook.com>
2019-07-21 12:44:21 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     google: {
-        clientID: "190122539728-243uunrtg6g0o8tie8f09qpr01mf163t.apps.googleusercontent.com",
-        clientSecret: "pwVTASyzZdCDmf8Ra3_LTto4"
+        clientID: "GOOGLE_CLIENT_ID",
+        clientSecret: "GOOOGLE_CLIENT_SECRET"
     },
     mongodb: {
         dbURI: "mongodb+srv://praveen123:praveen123@mern-d5kl2.mongodb.net/test?retryWrites=true&w=majority"
